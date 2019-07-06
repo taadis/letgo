6b9070355cc69a3eef2e5da750a8170238803d85
taadis <i@taadis.com>
2019-07-06 15:38:31 +0800

    add ai-go

---
+++ example/ai-go/main.go
@@ -0,0 +1,23 @@
+// ai-go project main.go
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// AI 核心代码, 估值3个亿
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		input := scanner.Text()
+		input = strings.ReplaceAll(input, "吗", "")
+		input = strings.ReplaceAll(input, "?", "!")
+		input = strings.ReplaceAll(input, "？", "!")
+		fmt.Println(input)
+		fmt.Println()
+	}
+}
