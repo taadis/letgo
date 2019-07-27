b673c67c421d2b7c5855782bf45383bbd38f7528
taadis <i@taadis.com>
2019-07-27 19:20:57 +0800

    update example wechat_dat_to_jpg.go and .gitignore

--- _examples/others/wechat_dat_to_jpg.go
+++ _examples/others/wechat_dat_to_jpg.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
 	"strings"
 	//"os"
 )
@@ -15,16 +17,22 @@ func init() {
 }
 
 func main() {
-	fmt.Println("wechat_dat_to_jpg.go")
 	// 微信 dat 文件目录
 	// C:\Users\taadis\Documents\WeChat Files\taadis\FileStorage\Image\2019-07
 
-	// 源目录
-	dirname := `E:\\dats`
+	// 输入目录
+	dirname, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Panicln(err)
+	}
+	// 输出目录
+	outputDirname := filepath.Join(dirname, "output")
+	log.Printf("当前程序执行路径: %s", dirname)
 	fileInfos, err := ioutil.ReadDir(dirname)
 	if err != nil {
 		log.Panicln(err)
 	}
+	var datFiles []string // dat 文件集合
 	for key, fileInfo := range fileInfos {
 		// 判断是目录还是文件
 		if fileInfo.IsDir() {
@@ -34,26 +42,56 @@ func main() {
 			if !strings.HasSuffix(fileInfo.Name(), ".dat") {
 				break
 			}
-
 			// 读取文件字节数据
 			inFilename := fmt.Sprintf("%s\\%s", dirname, fileInfo.Name())
-			dat, err := ioutil.ReadFile(inFilename) //
-			if err != nil {
-				log.Println(err)
-			}
-
-			img, err := WechatDatToImage(dat)
-			if err != nil {
-				log.Panicln(err)
-			}
-
-			outFilename := fmt.Sprintf("%s.jpg", inFilename)
-			err = ioutil.WriteFile(outFilename, img, 0777)
-			if err != nil {
-				log.Panicln(err)
-			}
-			log.Printf("compeleted: %d %s", key, outFilename)
+			datFiles = append(datFiles, inFilename)
 		}
+	}
+
+	//
+	if len(datFiles) < 1 {
+		log.Println("当前路径下没有找到需要处理的 .dat 文件")
+		return
+	}
+
+	for key, fileInfo := range fileInfos {
+		// 判断是目录还是文件
+		if fileInfo.IsDir() {
+			log.Println("dir: ", key)
+		} else {
+			// 判断是不是 .dat 后缀的文件
+			if !strings.HasSuffix(fileInfo.Name(), ".dat") {
+				break
+			}
+			// 读取文件字节数据
+			inFilename := fmt.Sprintf("%s\\%s", dirname, fileInfo.Name())
+			datFiles = append(datFiles, inFilename)
+		}
+	}
+
+	//
+	for key, datFile := range datFiles {
+		datBytes, err := ioutil.ReadFile(datFile) //
+		if err != nil {
+			log.Println(err)
+		}
+
+		imgBytes, err := WechatDatToImage(datBytes)
+		if err != nil {
+			log.Panicln(err)
+		}
+
+		datFilename := filepath.Base(datFile) + ".jpg"
+		outFilename := filepath.Join(outputDirname, datFilename)
+		err = ensureDirExist(outFilename)
+		if err != nil {
+			log.Panicln(err)
+		}
+		err = ioutil.WriteFile(outFilename, imgBytes, 0777)
+		if err != nil {
+			log.Panicln(err)
+		}
+		log.Printf("compeleted: %d %s", key, outFilename)
 	}
 }
 
@@ -69,4 +107,25 @@ func WechatDatToImage(dat []byte) (img []byte, err error) {
 	}
 	img = buffer.Bytes()
 	return
+}
+
+// 写个函数，确保文件夹存在，省的重复写
+func ensureDirExist(path string) error {
+	dir := filepath.Dir(path)
+	exists := isPathExists(dir)
+	if !exists {
+		err := os.MkdirAll(dir, os.ModePerm)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// 判断路径是否存在
+func isPathExists(path string) bool {
+	if _, err := os.Stat(path); err != nil {
+		return false
+	}
+	return true
 }
