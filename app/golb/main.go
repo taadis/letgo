afe0009fde1fc0fe373583a71e36c3192980c09e
taadis <i@taadis.com>
2019-11-04 15:29:11 +0800

    update golb

--- app/golb/main.go
+++ app/golb/main.go
@@ -1,10 +1,17 @@
-// main.go
+// app/golb/main.go
 package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("golb app")
+	fmt.Println("ready to start golb app")
+	mux := http.NewServeMux()
+	err := http.ListenAndServe(":5903", mux)
+	if err != nil {
+		fmt.Errorf("start golb error: ", err.Error())
+	}
+	//fmt.Println("golb running in: ", "http://localhost:5903")
 }
