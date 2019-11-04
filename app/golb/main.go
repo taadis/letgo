c1333beeeb9b8378960098a7162f643170c0ec84
taadis <i@taadis.com>
2019-11-04 15:52:46 +0800

    update golb app

--- app/golb/main.go
+++ app/golb/main.go
@@ -4,11 +4,14 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/taadis/letgo/handler/company"
 )
 
 func main() {
 	fmt.Println("ready to start golb app")
 	mux := http.NewServeMux()
+	mux.HandleFunc("/", company.Index)
 	err := http.ListenAndServe(":5903", mux)
 	if err != nil {
 		fmt.Errorf("start golb error: ", err.Error())
