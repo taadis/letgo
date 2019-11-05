ee6a120003d67f66963ef2fc53b03b2aa5952d37
taadis <i@taadis.com>
2019-11-05 15:20:31 +0800

    add panic recover

--- handler/user/user_handler.go
+++ handler/user/user_handler.go
@@ -1,13 +1,16 @@
 package user
 
 import (
-	"errors"
+	"encoding/json"
 	"fmt"
 
+	//"path"
 	//"html/template"
 	//"io"
 	"net/http"
+
 	//"github.com/taadis/letgo/store"
+	"github.com/taadis/letgo/store/mysql"
 )
 
 func HandleFunc(w http.ResponseWriter, r *http.Request) {
@@ -33,7 +36,29 @@ func HandleFunc(w http.ResponseWriter, r *http.Request) {
 //
 func getFunc(w http.ResponseWriter, r *http.Request) (err error) {
 	//w.WriteHeader(http.StatusNotImplemented)
-	err = errors.New("get user error")
+	//err = errors.New("get user error")
+	defer func() {
+		if rc := recover(); rc != nil {
+			s := fmt.Sprintf("panic recover %v", rc)
+			fmt.Println(s)
+			http.Error(w, s, http.StatusInternalServerError)
+		}
+	}()
+	values := r.URL.Query()
+	id := values.Get("id")
+	panic("鬼知道为什么崩溃了!")
+	//user := store.User{}
+	userStore := mysql.UserStore{}
+	user, err := userStore.User(id)
+	if err != nil {
+		return
+	}
+	bytes, err := json.Marshal(user)
+	if err != nil {
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(bytes)
 	return
 }
 
