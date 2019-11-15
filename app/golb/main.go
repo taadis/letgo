1076d8b77abed0adbe631fc340053447df6e1403
taadis <i@taadis.com>
2019-11-15 15:35:30 +0800

    add index views

--- app/golb/main.go
+++ app/golb/main.go
@@ -3,24 +3,35 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
-
-	"github.com/taadis/letgo/handler/company"
-	"github.com/taadis/letgo/handler/home"
-	"github.com/taadis/letgo/handler/middleware"
-	"github.com/taadis/letgo/handler/user"
 )
+
+// User struct
+type User struct {
+	UserName string
+}
 
 func main() {
 	fmt.Println("ready to start golb app")
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home.HandleFunc)
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		user := User{
+			UserName: "taadis",
+		}
+		tmp, err := template.ParseFiles("views/index.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		err = tmp.Execute(w, &user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
 	//mux.HandleFunc("/system/user/", user.HandleFunc)
-	mux.Handle("/system/user/", midleware.PanicAndRecover(http.HandlerFunc(user.HandleFunc)))
-	mux.HandleFunc("/company/", company.HandleFunc)
+	//mux.Handle("/system/user/", midleware.PanicAndRecover(http.HandlerFunc(user.HandleFunc)))
 	err := http.ListenAndServe(":5903", mux)
 	if err != nil {
 		fmt.Errorf("start golb error: ", err.Error())
 	}
-	//fmt.Println("golb running in: ", "http://localhost:5903")
 }
