5256936dde11e83a179f166bdc1715fc8abfc543
taadis <i@taadis.com>
2019-11-26 13:54:41 +0800

    add register.go

---
+++ app/golb/vm/register.go
@@ -0,0 +1,33 @@
+// register.go
+package vm
+
+import (
+	"fmt"
+
+	"github.com/taadis/letgo/app/golb/models"
+)
+
+type RegisterViewModel struct {
+	BaseViewModel
+}
+
+type RegisterViewModelOp struct {
+}
+
+func (RegisterViewModelOp) GetVM() RegisterViewModel {
+	v := RegisterViewModel{}
+	v.SetTitle("Register")
+	return v
+}
+
+func CheckUserName(username string) bool {
+	_, err := models.GetUserByUsername(username)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func AddUser(username, password, email string) error {
+	return models.AddUser(username, password, email)
+}
