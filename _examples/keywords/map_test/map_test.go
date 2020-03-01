c1f3120020de11e163c392e3f116247f74f0086f
taadis <i@taadis.com>
2020-03-01 22:09:55 +0800

    add map test

---
+++ _examples/keywords/map_test/map_test.go
@@ -0,0 +1,15 @@
+package map_test
+
+import (
+	"testing"
+)
+
+// 测试 map 的声常用明写法
+func TestMapDefine(t *testing.T) {
+	m := map[string]int{}
+	t.Log(m)
+	m1 := map[string]int{"1": 1, "2": 2, "3": 3}
+	t.Log(m1)
+	m2 := make(map[string]int, 10)
+	t.Log(m2)
+}
