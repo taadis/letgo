11da48d917f876739b770c1510a8301f7271e6cb
taadis <i@taadis.com>
2020-03-01 22:20:30 +0800

    update map test

--- _examples/keywords/map_test/map_test.go
+++ _examples/keywords/map_test/map_test.go
@@ -23,3 +23,20 @@ func TestMapSet(t *testing.T) {
 	m["1"] = 200
 	t.Log(m)
 }
+
+// 测试 map 的取值操作
+func TestMapGet(t *testing.T) {
+	m := map[string]int{}
+	t.Log(m)
+	value := m["0"] // 取值时没有对应的key, 则取值为类型对应的默认零值
+	t.Log(value)
+	// 但有个问题是当我们设置了一个 key, 赋值为零值时,
+	// 如何区分是有key 还是没有key呢?
+	value, ok := m["0"]
+	if ok != true {
+		t.Log("not key")
+	} else {
+		t.Log("has key")
+	}
+
+}
