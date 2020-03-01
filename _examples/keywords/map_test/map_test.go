82866ca6d77e074d421faca7f335288915529c24
taadis <i@taadis.com>
2020-03-01 22:12:56 +0800

    update map test

--- _examples/keywords/map_test/map_test.go
+++ _examples/keywords/map_test/map_test.go
@@ -13,3 +13,13 @@ func TestMapDefine(t *testing.T) {
 	m2 := make(map[string]int, 10)
 	t.Log(m2)
 }
+
+// 测试 map 的赋值操作
+func TestMapSet(t *testing.T) {
+	m := map[string]int{}
+	t.Log(m)
+	m["1"] = 100
+	t.Log(m)
+	m["1"] = 200
+	t.Log(m)
+}
