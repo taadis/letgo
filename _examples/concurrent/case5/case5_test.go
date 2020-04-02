079b61e1a13a18b68f5340378ae8756b08d69fca
taadis <i@taadis.com>
2020-04-02 16:24:29 +0800

    update case5 test

--- _examples/concurrent/case5/case5_test.go
+++ _examples/concurrent/case5/case5_test.go
@@ -1,19 +1,25 @@
 package case5
 
 import (
+	"fmt"
 	"runtime"
 	"testing"
 	"time"
 )
 
 func TestFirstResult(t *testing.T) {
-	ch := make(chan string, 5)
+	t.Logf("NumGoroutine: %d", runtime.NumGoroutine())
+	t.Log(ping())
+	//time.Sleep(3 * time.Second)
+	t.Logf("NumGoroutine: %d", runtime.NumGoroutine())
+}
+
+func ping() string {
+	ch := make(chan string, 1)
 	go pingBaidu(ch)
 	go pingBing(ch)
 	go pingGoogle(ch)
-	t.Logf("NumGoroutine: %d", runtime.NumGoroutine())
-	t.Log(<-ch)
-	t.Logf("NumGoroutine: %d", runtime.NumGoroutine())
+	return <-ch
 }
 
 func pingBaidu(ch chan string) {
@@ -29,4 +35,28 @@ func pingBing(ch chan string) {
 func pingGoogle(ch chan string) {
 	time.Sleep(3 * time.Second)
 	ch <- "from google"
+}
+
+func runTask(i int) string {
+	time.Sleep(10 * time.Millisecond)
+	return fmt.Sprintf("the result is from %d", i)
+}
+
+func FirstResponse() string {
+	numOfRunner := 10
+	ch := make(chan string, numOfRunner)
+	for i := 0; i < numOfRunner; i++ {
+		go func(i int) {
+			task := runTask(i)
+			ch <- task
+		}(i)
+	}
+	return <-ch
+}
+
+func TestFirst(t *testing.T) {
+	fmt.Println("Go num: ", runtime.NumGoroutine())
+	FirstResponse()
+	time.Sleep(5 * time.Second)
+	fmt.Println("Go num: ", runtime.NumGoroutine())
 }
