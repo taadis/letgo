b4570a23a19459907ebf3695a2f4010d183bce26
taadis <i@taadis.com>
2020-04-03 09:36:11 +0800

    update slice test

--- _examples/basic/slice_test.go
+++ _examples/basic/slice_test.go
@@ -8,9 +8,9 @@ func TestSlice1(t *testing.T) {
 	s1 := []int{1, 2, 3}
 	t.Log(s1)
 
-	s2 := ([3]int{1, 2, 3})[:]
+	//s2 := ([3]int{1, 2, 3})[:]
 	// invalid operation [3]int literal[:] (slice of unaddressable value)
-	t.Log(s2)
+	//t.Log(s2)
 
 	s3 := s1[:]
 	t.Log(s3)
