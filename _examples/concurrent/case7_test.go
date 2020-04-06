56529356e24942b94127aad2006a2b3374f8b585
taadis <i@taadis.com>
2020-04-06 13:40:44 +0800

    update concurrent case7 test

--- _examples/concurrent/case7_test.go
+++ _examples/concurrent/case7_test.go
@@ -1,7 +1,6 @@
 package concurrent
 
 import (
-	"fmt"
 	"sync"
 	"testing"
 )
@@ -13,27 +12,24 @@ func TestBreakfastShop(t *testing.T) {
 	ch := make(chan int, 100)
 	wg := sync.WaitGroup{}
 	wg.Add(2)
-	go Boss(&wg, ch)
-	go BossWife(&wg, ch)
+
+	// 老板在后厨造包子
+	go func(waitGroup *sync.WaitGroup, channel chan int) {
+		for i := 1; i <= 100; i++ {
+			t.Logf("老板造出了第 %d 个包子\n", i)
+			channel <- i
+		}
+		waitGroup.Done()
+	}(&wg, ch)
+
+	// 老板娘在前台卖包子
+	go func(waitGroup *sync.WaitGroup, channel chan int) {
+		for i, _ := <-channel; i <= 100; i++ {
+			t.Logf("老板娘卖出了第 %d 个包子", i)
+		}
+		waitGroup.Done()
+	}(&wg, ch)
+
 	wg.Wait()
 	t.Log("卖光啦, 早餐店收摊了.")
-}
-
-// 老板
-// 在后厨造包子
-func Boss(wg *sync.WaitGroup, ch chan int) {
-	for i := 0; i < 100; i++ {
-		fmt.Printf("老板造出了第 %d 个包子\n", i)
-		ch <- i
-	}
-	wg.Done()
-}
-
-// 老板娘
-// 在前台卖包子
-func BossWife(wg *sync.WaitGroup, ch chan int) {
-	for i, _ := <-ch; i < 100; i++ {
-		fmt.Printf("老板娘卖出了第 %d 个包子\n", i)
-	}
-	wg.Done()
 }
