fadca9a3228235726a18c32ea8f278d53ed9a170
taadis <i@taadis.com>
2020-04-06 11:45:43 +0800

    update concurrent case7 test

--- _examples/concurrent/case7_test.go
+++ _examples/concurrent/case7_test.go
@@ -10,7 +10,7 @@ import (
 func TestBreakfastShop(t *testing.T) {
 	t.Log("早餐店开门啦.")
 	// 声明一个信道
-	ch := make(chan int, 1000)
+	ch := make(chan int, 100)
 	wg := sync.WaitGroup{}
 	wg.Add(2)
 	go Boss(&wg, ch)
@@ -22,7 +22,7 @@ func TestBreakfastShop(t *testing.T) {
 // 老板
 // 在后厨造包子
 func Boss(wg *sync.WaitGroup, ch chan int) {
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 100; i++ {
 		fmt.Printf("老板造出了第 %d 个包子\n", i)
 		ch <- i
 	}
@@ -32,7 +32,7 @@ func Boss(wg *sync.WaitGroup, ch chan int) {
 // 老板娘
 // 在前台卖包子
 func BossWife(wg *sync.WaitGroup, ch chan int) {
-	for i, _ := <-ch; i < 1000; i++ {
+	for i, _ := <-ch; i < 100; i++ {
 		fmt.Printf("老板娘卖出了第 %d 个包子\n", i)
 	}
 	wg.Done()
