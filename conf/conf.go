27bd5d0e9e8cd8daf2487463357bc78bac79e884
taadis <i@taadis.com>
2020-06-20 13:59:29 +0800

    update database conf

--- conf/conf.go
+++ conf/conf.go
@@ -7,6 +7,8 @@ type DatabaseConf struct {
 
 	// format "username:password@(ip:port)/database?charset=utf8&parseTime=True&loc=Local"
 	DataSourceName string `json:"dataSourceName"`
+
+	LogMode bool `json:"logMode"`
 }
 
 // Database
@@ -14,5 +16,6 @@ func Database() *DatabaseConf {
 	return &DatabaseConf{
 		DriverName:     "mysql",
 		DataSourceName: "root:root..@/letgo?charset=utf8&parseTime=True&loc=Local",
+		LogMode:        true,
 	}
 }
