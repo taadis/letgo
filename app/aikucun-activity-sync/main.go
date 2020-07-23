6407cec0e025ad04055bea9e550915165741ceb6
taadis <i@taadis.com>
2020-07-23 15:45:20 +0800

    add cron

--- app/aikucun-activity-sync/main.go
+++ app/aikucun-activity-sync/main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/robfig/cron/v3"
 )
 
 func main() {
 	fmt.Println("aikucun-activity-sync")
+	c := cron.New()
+	spec := "* * * * *"
+	entryId, err := c.AddFunc(spec, ActivitiList)
+	if err != nil {
+		fmt.Println(".AddFunc error:", err.Error())
+		return
+	}
+	fmt.Println("entryId:", entryId)
+	c.Start()
+	defer c.Stop()
+	select {}
+}
+
+// ActivitiList
+func ActivitiList() {
+	fmt.Println("synching")
 }
