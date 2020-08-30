8e2867c10067ab4e3ae573fcaf9be2628be035b2
taadis <i@taadis.com>
2020-08-30 22:06:33 +0800

    add complex64 complex128 sizeof

--- _examples/basic/datatype_test.go
+++ _examples/basic/datatype_test.go
@@ -16,6 +16,9 @@ func TestSizeof(t *testing.T) {
 	var i64 int64
 	var f32 float32
 	var f64 float64
+	var cx64 complex64
+	var cx128 complex128
+
 	t.Log("数据类型 存储大小(单位:字节 byte)")
 	t.Log("bool", unsafe.Sizeof(b))
 	t.Log("string", unsafe.Sizeof(s))
@@ -26,6 +29,8 @@ func TestSizeof(t *testing.T) {
 	t.Log("int64", unsafe.Sizeof(i64))
 	t.Log("float32", unsafe.Sizeof(f32))
 	t.Log("float64", unsafe.Sizeof(f64))
+	t.Log("complex64", unsafe.Sizeof(cx64))
+	t.Log("complex128", unsafe.Sizeof(cx128))
 	// ...
 }
 
