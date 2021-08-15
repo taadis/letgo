cc4805a00bfe594564ff10b82e103a1f08f87730
taadis <i@taadis.com>
2021-08-15 20:27:33 +0800

    add learn_c_on_mac/_11_03_func_ptr

---
+++ learn_c_on_mac/_11_03_func_ptr/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int SquareIt(int num);
+
+int main(int argc, const char * argv[]) {
+    int (*myFuncPtr)(int);
+    int num = 5;
+    int result;
+
+    myFuncPtr = SquareIt;
+
+    result = (*myFuncPtr)(num);
+    printf("Using form (*myFuncPtr)(num) ... %d squared is %d.\n", num, result);
+
+    result = myFuncPtr(num);
+    printf("Using form myFuncPtr(num) ...... %d squared is %d.\n", num, result);
+
+    return 0;
+}
+
+int SquareIt(int num) {
+    return(num *num);
+}
+
