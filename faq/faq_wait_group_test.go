9a62b95c8a87f1ea0d4e3a6ec30157710796b9ff
taadis <i@taadis.com>
2022-01-21 16:51:02 +0800

    add TestSyncWait3_urls

--- faq/faq_wait_group_test.go
+++ faq/faq_wait_group_test.go
@@ -1,9 +1,38 @@
 package faq
 
 import (
+	"net/http"
 	"sync"
 	"testing"
 )
+
+// HTTP请求不同耗时的url,来验证sync.WaitGroup的使用
+func TestSyncWait3_urls(t *testing.T) {
+	wg := &sync.WaitGroup{}
+
+	urls := []string{
+		"https://baidu.com",
+		"https://google.com",
+		"https://bing.com",
+	}
+
+	for _, url := range urls {
+		wg.Add(1)
+		go func(wg *sync.WaitGroup, url string) {
+			defer wg.Done()
+
+			resp, err := http.Get(url)
+			if err != nil {
+				t.Logf("HTTP GET %s -> error:%v", url, err)
+				return
+			}
+			t.Logf("HTTP GET %s -> resp:%v, error:%v", url, resp.StatusCode, err)
+		}(wg, url)
+	}
+
+	wg.Wait()
+	t.Logf("finished")
+}
 
 // 如果wg.Add调用维护的计数更改成了负数,将产生panic
 func TestSyncWaitGroupPanic1(t *testing.T) {
