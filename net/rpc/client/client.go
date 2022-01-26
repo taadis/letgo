ea7b019b5904672fc165c4b2f4423ae8c7e2a254
taadis <i@taadis.com>
2022-01-26 16:23:46 +0800

    fix import and lint

--- net/rpc/client/client.go
+++ net/rpc/client/client.go
@@ -1,22 +1,23 @@
 package client
 
 import (
-	"gitee.com/taadis/letgo/net/rpc/server"
 	"log"
 	"net/rpc"
+
+	"github.com/taadis/letgo/net/rpc/server"
 )
 
-func Run(){
-	client, err := rpc.DialHTTP("tcp",   "localhost:1234")
+func Run() {
+	client, err := rpc.DialHTTP("tcp", "localhost:1234")
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
 
 	// 同步调用
-	args := &server.Args{7,8}
+	args := &server.Args{7, 8}
 	var reply int
 	err = client.Call("Arith.Multiply", args, &reply)
-	if err != nil{
+	if err != nil {
 		log.Println("client.Call Arith.Multiply error:", err)
 	}
 	log.Printf("Arith: %d*%d=%d", args.A, args.B, reply)
