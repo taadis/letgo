00fcc8127421a09918eef9af4b527f76cd57cb83
taadis <i@taadis.com>
2022-03-20 15:16:41 +0800

    clock:refresh inline

--- app/clock/main.go
+++ app/clock/main.go
@@ -9,6 +9,7 @@ func main() {
 	// 创建ticker,并设置多长时间触发一次
 	ticker := time.NewTicker(time.Second * 1)
 	for t := range ticker.C {
-		fmt.Printf("now time %v\n", t.Local().String())
+		//fmt.Printf("\r\033[know time %v", t.Local().String()) // \r\033]k is ok
+		fmt.Printf("now time %v\r", t.Local().String()) // \r is ok
 	}
 }
