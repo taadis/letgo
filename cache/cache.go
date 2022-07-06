2ef6541c893da984f9e5810f65024714102186ac
taadis <i@taadis.com>
2022-07-06 14:59:18 +0800

    todo:add cache options

--- cache/cache.go
+++ cache/cache.go
@@ -2,6 +2,7 @@ package cache
 
 import (
 	"context"
+	"crypto/tls"
 	"errors"
 	"time"
 )
@@ -15,4 +16,38 @@ type Cache interface {
 	Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error
 	Get(ctx context.Context, key string) (interface{}, error)
 	Del(ctx context.Context, key string) error
+}
+
+type Options struct {
+	Timeout time.Duration
+	TLS     *tls.Config
+	Version string
+	Address string
+	// more...
+}
+
+type Option func(*Options)
+
+func WithTimeout(d time.Duration) Option {
+	return func(o *Options) {
+		o.Timeout = d
+	}
+}
+
+func WithTLS(c *tls.Config) Option {
+	return func(o *Options) {
+		o.TLS = c
+	}
+}
+
+func WithVersion(v string) Option {
+	return func(o *Options) {
+		o.Version = v
+	}
+}
+
+func WithAddress(addr string) Option {
+	return func(o *Options) {
+		o.Address = addr
+	}
 }
