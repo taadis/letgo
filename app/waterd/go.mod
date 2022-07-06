2cb2a6c9467927b2098f788b6206c0a3d9d7a5ba
taadis <i@taadis.com>
2022-07-06 16:58:20 +0800

    refactor cron of waterd app

---
+++ app/waterd/go.mod
@@ -0,0 +1,5 @@
+module github.com/taadis/app/waterd
+
+go 1.11
+
+require github.com/robfig/cron/v3 v3.0.1
--- app/waterd/main.go
+++ app/waterd/main.go
@@ -4,23 +4,46 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"strings"
+	"syscall"
+	"time"
 
 	"github.com/robfig/cron/v3"
 )
 
 // main
 func main() {
-	c := cron.New()
-	spec := "* * * * *"
-	entryId, err := c.AddFunc(spec, Send)
-	if err != nil {
-		log.Fatalln("cron.AddFunc error:", err.Error())
+	// [spec]func
+	// e.g.[* * * * * *]print
+	jobs := make(map[string]func())
+	jobs["* * * * * *"] = print
+	jobs["*/5 * * * * *"] = fooPanic
+	jobs["*/5 * * * * *"] = fooSkip
+
+	logger := cron.VerbosePrintfLogger(log.Default())
+	c := cron.New(
+		cron.WithChain(
+			cron.Recover(logger),
+			cron.SkipIfStillRunning(logger),
+		),
+		cron.WithSeconds(),
+		cron.WithLogger(logger),
+	)
+
+	for spec, job := range jobs {
+		entryId, err := c.AddFunc(spec, job)
+		if err != nil {
+			log.Fatalln("cron.AddFunc error:", err.Error())
+		}
+		log.Println("added cron func entryId:", entryId)
+		c.Start()
 	}
-	log.Println("entryId:", entryId)
-	c.Start()
+
 	defer c.Stop()
-	select {}
+
+	waitExit()
 }
 
 // print
@@ -28,7 +51,40 @@ func print() {
 	log.Println("waterd")
 }
 
-// Send
+// cron调度时上一个job没执行完,下一个又开始跑了?跳过
+func fooSkip() {
+	log.Println("fooSkip start...")
+	time.Sleep(10 * time.Second)
+	log.Println("fooSkip ...end")
+}
+
+// cron调度的goroutine发生panic时会导致整个程序挂掉?
+// 可以通过cron.Recover统一来处理
+func fooPanic() {
+	panic("some panic")
+	log.Println("panic log")
+}
+
+func waitExit() {
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, syscall.SIGINT, syscall.SIGTERM)
+	for {
+		select {
+		case s := <-signalCh:
+			switch s {
+			case syscall.SIGINT:
+				fallthrough
+			case syscall.SIGTERM:
+				fallthrough
+			default:
+				log.Print("stop signal received, stop all scheduler")
+				log.Print("gracefully exit")
+				return
+			}
+		}
+	}
+}
+
 func Send() {
 	webhook := "https://oapi.dingtalk.com/robot/send?access_token=xxx"
 	url := webhook
