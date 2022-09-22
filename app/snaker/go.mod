f68e27a494b525cb1167ec6353bf061d8c28f3f4
taadis <i@taadis.com>
2022-09-22 14:52:26 +0800

    add app snaker

---
+++ app/snaker/go.mod
@@ -0,0 +1,5 @@
+module github.com/taadis/letgo/app/snaker
+
+go 1.11
+
+require fyne.io/fyne/v2 v2.2.3
---
+++ app/snaker/main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"image/color"
+	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+)
+
+var head *canvas.Rectangle
+
+type snakePart struct {
+	x, y float32
+}
+
+var (
+	snakeParts []snakePart
+	game       *fyne.Container
+)
+
+type moveType int
+
+const (
+	moveUp moveType = iota
+	moveDown
+	moveLeft
+	moveRight
+)
+
+var move = moveUp
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("贪吃蛇")
+	game = setupGame()
+	w.Resize(fyne.NewSize(400, 400))
+	w.SetFixedSize(true)
+	//设置键盘事件
+	w.Canvas().SetOnTypedKey(keyTyped)
+	w.SetContent(game)
+	go runGame()
+	w.ShowAndRun()
+}
+
+func setupGame() *fyne.Container {
+	var segments []fyne.CanvasObject
+
+	for i := 0; i < 10; i++ {
+		seg := snakePart{9, float32(5 + i)}
+		snakeParts = append(snakeParts, seg)
+
+		r := canvas.NewRectangle(&color.RGBA{G: 0x66, A: 0xff})
+		r.Resize(fyne.NewSize(10, 10))
+		r.Move(fyne.NewPos(90, float32(50+i*10)))
+		segments = append(segments, r)
+	}
+
+	// 添加头
+	head = canvas.NewRectangle(&color.RGBA{G: 0x66, A: 0xff})
+	head.Resize(fyne.NewSize(10, 10))
+	head.Move(fyne.NewPos(snakeParts[0].x*10, snakeParts[0].y*10))
+	segments = append(segments, head)
+
+	return container.NewWithoutLayout(segments...)
+}
+
+func runGame() {
+	nextPart := snakePart{snakeParts[0].x, snakeParts[0].y - 1}
+	for {
+		oldPos := fyne.NewPos(snakeParts[0].x*10, snakeParts[0].y*10)
+		newPos := fyne.NewPos(nextPart.x*10, nextPart.y*10)
+
+		canvas.NewPositionAnimation(oldPos, newPos, time.Millisecond*250,
+			func(p fyne.Position) {
+				head.Move(p)
+				canvas.Refresh(head)
+			}).Start()
+		end := len(snakeParts) - 1
+
+		canvas.NewPositionAnimation(fyne.NewPos(snakeParts[end].x*10, snakeParts[end].y*10),
+			fyne.NewPos(snakeParts[end-1].x*10, snakeParts[end-1].y*10), time.Millisecond*250,
+			func(p fyne.Position) {
+				tail := game.Objects[end]
+				tail.Move(p)
+				canvas.Refresh(tail)
+			}).Start()
+
+		time.Sleep(time.Millisecond * 250)
+		for i := len(snakeParts) - 1; i >= 1; i-- {
+			snakeParts[i] = snakeParts[i-1]
+		}
+
+		snakeParts[0] = nextPart
+		refreshGame()
+
+		switch move {
+		case moveUp:
+			nextPart = snakePart{nextPart.x, nextPart.y - 1}
+		case moveDown:
+			nextPart = snakePart{nextPart.x, nextPart.y + 1}
+		case moveLeft:
+			nextPart = snakePart{nextPart.x - 1, nextPart.y}
+		case moveRight:
+			nextPart = snakePart{nextPart.x + 1, nextPart.y}
+		}
+	}
+}
+
+func refreshGame() {
+	for i, seg := range snakeParts {
+		rect := game.Objects[i]
+		rect.Move(fyne.NewPos(seg.x*10, seg.y*10))
+	}
+	game.Refresh()
+}
+
+func keyTyped(e *fyne.KeyEvent) {
+	switch e.Name {
+	case fyne.KeyUp:
+		move = moveUp
+	case fyne.KeyDown:
+		move = moveDown
+	case fyne.KeyLeft:
+		move = moveLeft
+	case fyne.KeyRight:
+		move = moveRight
+	}
+}
