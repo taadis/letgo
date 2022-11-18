f42a7cc773607631d30c7b2ea9569e5a1195a16d
taadis <i@taadis.com>
2022-11-18 19:12:26 +0800

    add slice append test

--- _examples/basic/slice_test.go
+++ _examples/basic/slice_test.go
@@ -15,3 +15,38 @@ func TestSlice1(t *testing.T) {
 	s3 := s1[:]
 	t.Log(s3)
 }
+
+func TestSlice(t *testing.T) {
+	s := make([]int, 0, 10)
+	s = append(s, 0)
+	s = append(s, 1)
+	s = append(s, 2)
+	t.Logf("befor slice %+v %p", s, s)
+	AppendSlice(t, s)
+	t.Logf("after slice %+v %p %d %d", s, s, len(s), cap(s))
+}
+
+func AppendSlice(t *testing.T, s []int) {
+	t.Logf("befor append slice %+v %p %d %d", s, s, len(s), cap(s))
+	s[0] = 10
+	t.Logf("befor append slice %+v %p %d %d", s, s, len(s), cap(s))
+
+	s = append(s, 3)
+	s = append(s, 4)
+	s = append(s, 5)
+	t.Logf("after append slice %+v %p %d %d", s, s, len(s), cap(s))
+}
+
+func TestSlice2(t *testing.T) {
+	s := make([]int, 0)
+	s = append(s, 0)
+	s = append(s, 1)
+	s = append(s, 2)
+	t.Logf("befor slice %+v", s)
+	AppendSlice2(&s)
+	t.Logf("after slice %+v", s)
+}
+
+func AppendSlice2(s *[]int) {
+	*s = append(*s, 3)
+}
