629bbf9738cf873fec9e38f0ef40d0b4329e84e4
taadis <i@taadis.com>
2022-11-25 15:42:49 +0800

    update SelectionSort comments and test

--- algorithm/selection_sort.go
+++ algorithm/selection_sort.go
@@ -1,15 +1,18 @@
 package algorithm
 
-// SelectionSort
-func SelectionSort(values []int) []int{
-	for i, n := 0, len(values) ;i < n; i++{
+// SelectionSort 选择排序
+// O(N^2/2):嵌套循环
+//选择排序的步数大约只有冒泡排序的一般
+// 也就是说选择排序比冒泡排序快一倍左右
+func SelectionSort(values []int) []int {
+	for i, n := 0, len(values); i < n; i++ {
 		min := i
-		for j := i + 1; j < n; j ++{
+		for j := i + 1; j < n; j++ {
 			if values[min] > values[j] {
 				min = j
 			}
 		}
-		values[i], values[min]= values[min], values[i]
+		values[i], values[min] = values[min], values[i]
 	}
 	return values
 }
--- algorithm/selection_sort_test.go
+++ algorithm/selection_sort_test.go
@@ -7,12 +7,30 @@ import (
 )
 
 func TestSelectionSort(t *testing.T) {
+	t.Run("empty", func(t *testing.T) {
+		var values []int
+		sortedValues := SelectionSort(values)
+		t.Log(sortedValues)
+	})
+
+	t.Run("2613", func(t *testing.T) {
+		values := []int{2, 6, 1, 3}
+		sortedValues := SelectionSort(values)
+		t.Log(sortedValues)
+	})
+
+	t.Run("42713", func(t *testing.T) {
+		values := []int{4, 2, 7, 1, 3}
+		sortedValues := SelectionSort(values)
+		t.Log(sortedValues)
+	})
+
 	rand.Seed(time.Now().UnixNano())
 	var slice []int
-	for i := 0; i < 9; i++{
+	for i := 0; i < 9; i++ {
 		slice = append(slice, rand.Intn(100))
 	}
-	t.Log("Befor Selection Sort:", slice)
+	t.Log("before Selection Sort:", slice)
 	slice = SelectionSort(slice)
-	t.Log("After Selection Sort:", slice)
+	t.Log("after Selection Sort:", slice)
 }
