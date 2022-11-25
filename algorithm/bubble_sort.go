ddc0b93af75106faa1bc27c9abc10c8cf4a5cafc
taadis <i@taadis.com>
2022-11-25 13:33:58 +0800

    update TestBubbleSort

--- algorithm/bubble_sort.go
+++ algorithm/bubble_sort.go
@@ -1,6 +1,6 @@
 package algorithm
 
-// 冒泡排序
+// BubbleSort 冒泡排序
 func BubbleSort(values []int) []int {
 	for i, l := 0, len(values); i < l; i++ {
 		for j := 0; j < l-1; j++ {
--- algorithm/bubble_sort_test.go
+++ algorithm/bubble_sort_test.go
@@ -6,7 +6,15 @@ import (
 
 // 测试冒泡排序
 func TestBubbleSort(t *testing.T) {
-	values := []int{100, 1, 9, 8, 23, 5, 8, 7, 6}
-	result := BubbleSort(values)
-	t.Log(result)
+	t.Run("Empty", func(t *testing.T) {
+		var values []int
+		result := BubbleSort(values)
+		t.Log(result)
+	})
+
+	t.Run("BubbleSort", func(t *testing.T) {
+		values := []int{100, 1, 9, 8, 23, 5, 8, 7, 6}
+		result := BubbleSort(values)
+		t.Log(result)
+	})
 }
