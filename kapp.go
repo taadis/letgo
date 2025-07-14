efc9bb9825ac1e16521e7ce7a42e2ae9e51bea44
taadis <i@taadis.com>
2025-07-14 10:53:02 +0800

    add kotps

--- kapp.go
+++ kapp.go
@@ -5,5 +5,48 @@ import (
 )
 
 func (a *App) newkapp() *kratos.App {
-	return kratos.New()
+	opts := a.kopts()
+	kapp := kratos.New(opts...)
+	return kapp
+}
+
+func (a *App) kopts() []kratos.Option {
+	var opts []kratos.Option
+	if a.options.id != "" {
+		opts = append(opts, kratos.ID(a.options.id))
+	}
+	if a.options.name != "" {
+		opts = append(opts, kratos.Name(a.options.name))
+	}
+	if a.options.version != "" {
+		opts = append(opts, kratos.Version(a.options.version))
+	}
+	if a.options.metadata != nil {
+		opts = append(opts, kratos.Metadata(a.options.metadata))
+	}
+	if a.options.endpoints != nil {
+		opts = append(opts, kratos.Endpoint(a.options.endpoints...))
+	}
+	if a.options.ctx != nil {
+		opts = append(opts, kratos.Context(a.options.ctx))
+	}
+	if a.options.logger != nil {
+		opts = append(opts, kratos.Logger(a.options.logger))
+	}
+	if a.options.servers != nil {
+		opts = append(opts, kratos.Server(a.options.servers...))
+	}
+	if a.options.sigs != nil {
+		opts = append(opts, kratos.Signal(a.options.sigs...))
+	}
+	if a.options.registrar != nil {
+		opts = append(opts, kratos.Registrar(a.options.registrar))
+	}
+	if a.options.registrarTimeout != 0 {
+		opts = append(opts, kratos.RegistrarTimeout(a.options.registrarTimeout))
+	}
+	if a.options.stopTimeout != 0 {
+		opts = append(opts, kratos.StopTimeout(a.options.stopTimeout))
+	}
+	return opts
 }
