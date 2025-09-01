42d697f14ed108c1ef0d57b8d999a373a22c31d8
taadis <i@taadis.com>
2025-09-01 19:58:05 +0800

    add v.py

---
+++ v.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+# print(f"version:{tf.__version__}")
+
+print(f"tf.verison.COMPILER_VERSION={tf.version.COMPILER_VERSION}")
+
+print(f"tf.version.GIT_VERSION={tf.version.GIT_VERSION}")
+
+print(f"tf.version.GRAPH_DEF_VERSION={tf.version.GRAPH_DEF_VERSION}")
+
+print(f"tf.version.GRAPH_DEF_VERSION_MIN_CONSUMER={tf.version.GRAPH_DEF_VERSION_MIN_CONSUMER}")
+
+print(f"tf.version.GRAPH_DEF_VERSION_MIN_PRODUCER={tf.version.GRAPH_DEF_VERSION_MIN_PRODUCER}")
+
+print(f"tf.version.VERSION={tf.version.VERSION}")
