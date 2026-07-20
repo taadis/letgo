19002a84f45bc17ec0b2a8f9e54a5d317c24d24a
taadis <i@taadis.com>
2026-07-21 03:30:52 +0800

    add produce func

--- syncx/alternate_printer.go
+++ syncx/alternate_printer.go
@@ -4,12 +4,33 @@ import "fmt"
 
 type AlternatePrinter struct {
 	total int
+
+	ch chan int
 }
 
 func NewAlternatePrinter(total int) *AlternatePrinter {
-	return &AlternatePrinter{}
+	return &AlternatePrinter{
+		total: total,
+		ch:    make(chan int),
+	}
+}
+
+func (p *AlternatePrinter) produce() {
+	for i := 0; i <= p.total; i++ {
+		p.ch <- i
+	}
+
+	close(p.ch)
 }
 
 func (p *AlternatePrinter) Run() {
-	fmt.Printf("total is %d", p.total)
+	fmt.Printf("total is %d\n", p.total)
+
+	go p.produce()
+
+	for num := range p.ch {
+		fmt.Printf("%d\n", num)
+	}
+
+	fmt.Printf("all done.\n")
 }
